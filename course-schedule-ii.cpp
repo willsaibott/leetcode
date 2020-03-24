6219b1d3fd1121b1a498c8a85c2509eb309fafae
willsaibott <willsaibott@gmail.com>
2020-03-24 16:26:00 -0300

    Improvements and comments

--- course-schedule-ii.cpp
+++ course-schedule-ii.cpp
@@ -8,48 +8,73 @@
 
 class Solution {
 
-  bool deep_search(std::vector<int>&  result,
-                   int                node,
-                   std::vector<bool>& already_added,
-                   std::vector<bool>& bitmap)
+  /**
+   * DFS in the graph
+   *
+   * @param req_map       map of vertex
+   * @param result        result obj
+   * @param node          current node visited
+   * @param already_added bitmap indicating which elements have already
+   *                      been added to the result
+   * @param bitmap        current historical information of dfs stack to detect
+   *                      cyclic dependency
+   */
+  bool depth_first_search(const std::vector<std::vector<int>>& req_map,
+                          std::vector<int>&                    result,
+                          int                                  node,
+                          std::vector<bool>&                   already_added,
+                          std::vector<bool>&                   bitmap)
   {
     if (!already_added[node]) {
-        for (const auto& pre : prereq[node]) {
+        for (const auto& pre : req_map[node]) {
           if (!already_added[pre]) {
             // deadlock by cyclic dependency
             if (bitmap[pre]) {
               return false;
             }
             else {
+              // save historic
               bitmap[pre] = true;
-              if (!deep_search(result, pre, already_added, bitmap)) {
+
+              // if one cyclic dependency is detected, propagate it
+              if (!depth_first_search(req_map,
+                                      result,
+                                      pre,
+                                      already_added,
+                                      bitmap))
+              {
                 return false;
               }
             }
           }
         }
+        // flag it as added to result
         already_added[node] = true;
         result.push_back(node);
     }
     return true;
   }
 
-  std::unordered_map<int, std::vector<int>> prereq;
 public:
-    std::vector<int> findOrder(int numCourses, std::vector<std::vector<int>>& prerequisites) {
+    std::vector<int>
+    findOrder(int                            numCourses,
+              std::vector<std::vector<int>>& prerequisites)
+    {
+      std::vector<std::vector<int>> req_map(numCourses);
+      std::vector<bool> already_added(numCourses, false);
       std::vector<int> result;
       result.reserve(numCourses);
 
+      // build adj map
       for (const auto& req_pair : prerequisites) {
-          prereq[req_pair[0]].push_back(req_pair[1]);
+          req_map[req_pair[0]].push_back(req_pair[1]);
       }
 
-      std::vector<bool> already_added(numCourses, false);
-      std::vector<int> temp;
       for (int ii = 0; ii < numCourses; ii++) {
+        // if the node isn't already visited:
         if (!already_added[ii]) {
           std::vector<bool> bitmap(numCourses, false);
-          if (!deep_search(result, ii, already_added, bitmap)) {
+          if (!depth_first_search(req_map, result, ii, already_added, bitmap)) {
             result.clear();
             break;
           }
